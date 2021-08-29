62c655a7ca5fed2c95395d4595845b912adc72b5
Edolyne Long <ed@edolyne.com>
2021-08-28 20:08:51 -0400

    work on main product sections for homepage, and press

---
+++ assets/section-main-press.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $('.featured-press').slick({
+    slidesToShow: 2,
+    adaptiveHeight: false,
+    nextArrow: $('.slick-next'),
+    prevArrow: $('.slick-prev')
+  });
+});
